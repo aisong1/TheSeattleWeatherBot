83210dda420cfe6a343fff7eacf363b3a245dea2
Andrew Song <andrew_song61@hotmail.com>
2020-02-25 17:01:57 -0800

    Update time and date

--- src/main/java/SeattleWeatherBot.java
+++ src/main/java/SeattleWeatherBot.java
@@ -5,16 +5,18 @@ import twitter4j.TwitterFactory;
 import net.aksingh.owmjapis.core.OWM;
 import net.aksingh.owmjapis.model.CurrentWeather;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import twitter4j.conf.ConfigurationBuilder;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SeattleWeatherBot {
 
     public static void main(String[] args) {
 
         try {
-            // Create Twitter instance
-            // Twitter twitter = TwitterFactory.getSingleton();
 
             ConfigurationBuilder cb = new ConfigurationBuilder();
             cb.setDebugEnabled(true)
@@ -25,24 +27,36 @@ public class SeattleWeatherBot {
             TwitterFactory tf = new TwitterFactory(cb.build());
             Twitter twitter = tf.getInstance();
 
-            // Get environment variables
-            // Dotenv dotenv = Dotenv.load();
-
             OWM owm = new OWM(System.getenv("OWM_API_KEY"));
             owm.setUnit(OWM.Unit.IMPERIAL);
             CurrentWeather cwd = owm.currentWeatherByCityId(5809844);
 
+            // Check if weather information is available
+            double rain;
+            if (cwd.hasRainData() && cwd.getRainData().getPrecipVol3h() != null) {
+                rain = cwd.getRainData().getPrecipVol3h();
+            } else {
+                rain = 0;
+            }
 
-            double rain = (cwd.hasRainData() && cwd.getRainData().getPrecipVol3h() != null) ? cwd.getRainData().getPrecipVol3h() : 0;
+            // Get local time
+            LocalDateTime now = LocalDateTime.now();
+            ZoneId id = ZoneId.of("America/Los_Angeles");
+            ZonedDateTime zonedDateTime = ZonedDateTime.of(now, id);
+            String formattedDate = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm").format(zonedDateTime);
 
-            String dateTime = "[" + cwd.getDateTime().toString() + "] ";
+            // Update tweet
+            String dateTime = "[" + formattedDate + " PST] ";
             String currTemp = "It is currently " + Math.round(cwd.getMainData().getTemp()) + "˚F. ";
-            String minMaxTemp = "Today will have a max of " + Math.round(cwd.getMainData().getTempMax()) + "˚F and a min of "
+            String minMaxTemp;
+            minMaxTemp = "Today will have a max of " + Math.round(cwd.getMainData().getTempMax()) + "˚F and a min of "
                     + Math.round(cwd.getMainData().getTempMin()) + "˚F. ";
             String rain3H = "There has been " + rain + " mm of rain in the last three hours.";
             String message = dateTime + currTemp + minMaxTemp + rain3H;
+            System.out.println(message);
             Status status = twitter.updateStatus(message);
             System.out.println("Successfully updated the status to [" + status.getText() + "].");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
